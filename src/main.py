1c95bfbe18cab677a63398bc7c44c5086f312f80
Giovanni Stea <stea.giovanni.56@gmail.com>
2024-10-09 01:42:00 +0200

    Updated main with command line args

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
 
+import argparse
 from finetuning import CLIPFinetuner
 
 
 if __name__ == "__main__":
-    # finetuning the CLIP model
-    finetuner = CLIPFinetuner()
-    finetuner.fit(epochs=100, verbose=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ep", "--epochs", type=int, default=50)
+    parser.add_argument("-bs", "--batch_size", type=int, default=64)
+    parser.add_argument("-lr", "--learning_rate", type=float, default=5e-5)
+    parser.add_argument("--load", type=str, default=None)
+
+    args = parser.parse_args()
+
+    # finetuning
+    finetuner = CLIPFinetuner(
+        model_name="ViT-B/32",
+        batch_size=args.batch_size,
+        lr=args.learning_rate
+    )
+
+    if args.load:
+        finetuner.load_model(args.load)
+
+    finetuner.fit(epochs=args.epochs)    
