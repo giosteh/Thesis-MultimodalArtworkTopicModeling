9ed32af7d00f9fe6da9be113ef82c87249ac4ac6
Giovanni Stea <stea.giovanni.56@gmail.com>
2024-10-30 12:01:50 +0100

    Think to have completed artwork clustering

--- src/artwork_clustering.py
+++ src/artwork_clustering.py
@@ -5,13 +5,17 @@ Classes and functions for clustering artworks.
 import clip
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, Dataset
-from sklearn.cluster import MiniBatchKMeans
+from torch.utils.data import DataLoader
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.cluster import MiniBatchKMeans, DBSCAN
+from hdbscan import HDBSCAN
 import umap
 
 from clip_finetuning import ImageCaptionDataset
 
 from typing import List, Tuple
+from scipy.spatial.distance import cdist
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -22,19 +26,20 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 
-def load_finetuned_model(model_path: str) -> nn.Module:
+def load_model(base_model: str, model_path: str) -> nn.Module:
     """
     Loads the finetuned model.
 
     Args:
+        base_model (str): The base model to use.
         model_path (str): The path to the finetuned model.
 
     Returns:
         nn.Module: The finetuned model.
     """
-    model, _ = clip.load(model_path, device=device, jit=False)
-    checkpoint = torch.load(model_path, weights_only=True)
-    model.load_state_dict(checkpoint["model_state_dict"])
+    model, _ = clip.load(base_model, device=device, jit=False)
+    checkpoint = torch.load(model_path)
+    model.load_state_dict(checkpoint)
     model.float()
     model.eval()
 
@@ -45,7 +50,7 @@ class EmbeddingDatasetBuilder:
 
     def __init__(self,
                  base_model: str = "ViT-B/32",
-                 finetuned_model_path: str = "models/finetuned.pt",
+                 model_path: str = "models/finetuned.pt",
                  raw_dataset: ImageCaptionDataset = ImageCaptionDataset(raw_only=True),
                  dataset: ImageCaptionDataset = ImageCaptionDataset(),
                  use_base_model: bool = False) -> None:
@@ -54,7 +59,7 @@ class EmbeddingDatasetBuilder:
 
         Args:
             base_model (str): The base model to use. Defaults to "ViT-B/32".
-            finetuned_model_path (str): The path to the finetuned model. Defaults to "models/finetuned.pt".
+            model_path (str): The path to the finetuned model. Defaults to "models/finetuned.pt".
             raw_dataset (ImageCaptionDataset): The raw dataset. Defaults to ImageCaptionDataset(raw_only=True).
             dataset (ImageCaptionDataset): The dataset. Defaults to ImageCaptionDataset().
             use_base_model (bool): Whether to select the base embeddings. Defaults to False.
@@ -62,7 +67,7 @@ class EmbeddingDatasetBuilder:
         self._base_model, _ = clip.load(base_model, device=device, jit=False)
         self._base_model.float()
         self._base_model.eval()
-        self._finetuned_model = load_finetuned_model(finetuned_model_path)
+        self._finetuned_model = load_model(base_model, model_path)
 
         self._raw_loader = DataLoader(raw_dataset, batch_size=1, shuffle=False)
         self._data_loader = DataLoader(dataset, batch_size=1, shuffle=False)
@@ -102,7 +107,6 @@ class EmbeddingDatasetBuilder:
         
         if self._use_base_model:
             return pd.DataFrame(rows).rename(columns={"base_embedding": "embedding"})
-        
         return pd.DataFrame(rows).rename(columns={"finetuned_embedding": "embedding"})
 
 
@@ -110,7 +114,7 @@ class ArtworkClusterer:
 
     def __init__(self,
                  base_model: str = "ViT-B/32",
-                 finetuned_model_path: str = None,
+                 model_path: str = None,
                  dataset: pd.DataFrame = EmbeddingDatasetBuilder()(),
                  signifiers_path: str = "data/signifiers.pkl") -> None:
         """
@@ -118,110 +122,197 @@ class ArtworkClusterer:
 
         Args:
             base_model (str): The base model to use. Defaults to "ViT-B/32".
-            finetuned_model_path (str): The path to the finetuned model. Defaults to None.
+            model_path (str): The path to the finetuned model. Defaults to None.
             dataset (pd.DataFrame): The dataset. Defaults to EmbeddingDatasetBuilder()().
             signifiers_path (str): The path to the signifiers. Defaults to "data/signifiers.pkl".
+        
+        Returns:
+            None
         """
         self._model, _ = clip.load(base_model, device=device, jit=False)
         self._model.float()
         self._model.eval()
-        if finetuned_model_path:
-            self._model = load_finetuned_model(finetuned_model_path)
+        if model_path:
+            self._model = load_model(base_model, model_path)
         
         embeddings = dataset["embedding"].apply(lambda x: np.fromstring(x[1:-1], sep=","))
-        self._embeddings = np.vstack(embeddings.values)
+        embeddings = np.vstack(embeddings.values)
+        X_torch = torch.from_numpy(embeddings).float()
+        X_normalized = X_torch / X_torch.norm(dim=-1, keepdim=True)
+        self._embeddings = X_normalized.cpu().numpy()
+        self._labels = None
+        self._probabilities = None
 
         with open(signifiers_path, "rb") as f:
             self._signifiers = pickle.load(f)
     
 
-    def cluster(self, mode: str = "kmeans", n_clusters: int = 10, n_terms: int = 10) -> None:
+    def cluster(self,
+                method: str = "kmeans",
+                reduce_with: str = None,
+                represent_with: str = "centroid",
+                n_terms: int = 10,
+                **kwargs) -> None:
         """
         Clusters the embeddings using the specified mode.
 
         Args:
-            mode (str): The clustering mode. Defaults to "kmeans".
-            n_clusters (int): The number of clusters to use. Defaults to 10.
-            n_terms (int): The number of terms to assign to each cluster. Defaults to 10.
-        """
-        representatives, labels = None, None
-        match mode:
-            case "kmeans":
-                representatives, labels = self._cluster_with_kmeans(n_clusters)
-        
-        self._visualize_with_umap(labels)
-        interpretations = self._signify_clusters(representatives, n_terms)
+            method (str): The clustering method to use. Defaults to "kmeans".
+            reduce_with (str): The method to use for dimensionality reduction. Defaults to None.
+            represent_with (str): The method to use to represent the clusters. Defaults to "centroid".
+            n_terms (int): The number of terms to use. Defaults to 10.
 
-        # Saving the interpretations
-        with open("data/cluster_interpretations.pkl", "wb") as f:
-            pickle.dump(interpretations, f)
+        Returns:
+            None
+        """
+        clusterer = None
+        match method: # Clustering method
+            case "kmeans":
+                clusterer = MiniBatchKMeans(
+                    n_clusters=kwargs["n_clusters"] if "n_clusters" in kwargs else 10,
+                    metric="cosine",
+                    init="k-means++",
+                    n_init=10,
+                    max_iter=1000,
+                    random_state=42
+                )
+            case "dbscan":
+                clusterer = DBSCAN(
+                    eps=kwargs["eps"] if "eps" in kwargs else 0.2,
+                    min_samples=kwargs["min_samples"] if "min_samples" in kwargs else 5,
+                    metric="cosine",
+                    n_jobs=-1
+                )
+            case "hdbscan":
+                clusterer = HDBSCAN(
+                    min_cluster_size=kwargs["min_cluster_size"] if "min_cluster_size" in kwargs else 10,
+                    min_samples=kwargs["min_samples"] if "min_samples" in kwargs else 5,
+                    metric="cosine",
+                    cluster_selection_method="eom",
+                    prediction_data=True
+                )
+        
+        reducer = None
+        match reduce_with: # Dimensionality reduction
+            case "umap":
+                reducer = umap.UMAP(
+                    n_components=kwargs["n_components"] if "n_components" in kwargs else 2,
+                    n_neighbors=kwargs["n_neighbors"] if "n_neighbors" in kwargs else 15,
+                    min_dist=kwargs["min_dist"] if "min_dist" in kwargs else .1,
+                    random_state=42
+                )
+            case "tsne":
+                reducer = TSNE(
+                    n_components=kwargs["n_components"] if "n_components" in kwargs else 2,
+                    perplexity=kwargs["perplexity"] if "perplexity" in kwargs else 30,
+                    random_state=42
+                )
+            case "pca":
+                reducer = PCA(
+                    n_components=kwargs["n_components"] if "n_components" in kwargs else 2,
+                    random_state=42
+                )
+        if reducer:
+            self._embeddings = reducer.fit_transform(self._embeddings)
+        if clusterer:
+            self._labels = clusterer.fit_predict(self._embeddings)
+            if method == "hdbscan":
+                self._probabilities = clusterer.probabilities_
+        
+        cluster_reprs = self._get_cluster_reprs(method, represent_with)
+        interpretations = self._signify_clusters(cluster_reprs, n_terms)
+        # Saving and visualizing
+        with open("data/results.pkl", "wb") as f:
+            data = (self._labels, interpretations)
+            pickle.dump(data, f)
+        self._visualize(self._labels)
     
-    def _signify_clusters(self, representatives: List[torch.Tensor], n_terms: int = 10) -> List[List[(str, float)]]:
+    def _get_cluster_reprs(self, clustering_method: str, mode: str) -> List[torch.Tensor]:
+        """
+        Gets the cluster representatives.
+
+        Args:
+            clustering_method (str): The clustering method used ("centroid" or "medoid").
+            mode (str): The mode to use to get the cluster representatives.
+
+        Returns:
+            List[torch.Tensor]: The cluster representatives.
+        """
+        unique_labels = np.unique(self._labels[self._labels != -1])
+        cluster_reprs = []
+
+        for label in unique_labels:
+            mask = (self._labels == label)
+            points = self._embeddings[mask]
+            weights = self._probabilities[mask] if clustering_method == "hdbscan" else None
+
+            centroid = np.mean(points, axis=0)
+            if weights:
+                centroid = np.average(points, axis=0, weights=weights)
+            
+            if mode == "centroid":
+                cluster_reprs.append(torch.from_numpy(centroid).float())
+            else:
+                distances = cdist(points, points)
+                if weights:
+                    distances = distances * weights.reshape(-1, 1)
+                total_distances = np.sum(distances, axis=1)
+                medoid = points[total_distances.argmin()]
+
+                cluster_reprs.append(torch.from_numpy(medoid).float())
+        return cluster_reprs
+    
+    def _signify_clusters(self, cluster_reprs: List[torch.Tensor], n_terms: int = 10) -> List[List[(str, float)]]:
         """
         Signifies the clusters assigning the most similar labels to each centroid.
 
         Args:
-            representatives (List[torch.Tensor]): The cluster representatives.
-            n_labels (int): The number of labels to assign.
+            cluster_reprs (List[torch.Tensor]): The cluster representatives.
+            n_labels (int): The number of labels to assign. Defaults to 10.
 
         Returns:
             List[List[(str, float)]]: The list of labels for each centroid.
         """
-        cluster_interpretations = []
+        interpretations = []
         signifiers = torch.cat([clip.tokenize(f"a {s} painting") for s in self._signifiers]).to(device)
 
         with torch.no_grad():
-            for representative in representatives:
-                representative = representative.to(device)
+            for cluster_repr in cluster_reprs:
+                cluster_repr = cluster_repr.to(device)
                 signifiers = self._model.encode_text(signifiers)
                 signifiers = signifiers / signifiers.norm(dim=-1, keepdim=True)
 
-                similarity = (100 * representative @ signifiers.t()).softmax(dim=-1)
+                similarity = (100 * cluster_repr @ signifiers.t()).softmax(dim=-1)
                 values, indices = similarity[0].topk(n_terms)
                 interpretation = [(self._signifiers[i], v.item()) for i, v in zip(indices, values)]
 
-                cluster_interpretations.append(interpretation)
+                interpretations.append(interpretation)
 
-        return cluster_interpretations
+        return interpretations
 
-    def _visualize_with_umap(self, labels: np.ndarray, n_neighbors: int = 30, min_dist: float = .1) -> None:
+    def _visualize(self, labels: np.ndarray, n_neighbors: int = 15, min_dist: float = .1) -> None:
         """
         Visualizes the clusters using UMAP.
 
         Args:
             labels (np.ndarray): The labels.
-            n_neighbors (int): The number of neighbors to use for UMAP.
-            min_dist (float): The minimum distance to use for UMAP.
+            n_neighbors (int): The number of neighbors to use for UMAP. Defaults to 30.
+            min_dist (float): The minimum distance to use for UMAP. Defaults to .1.
 
         Returns:
             None
         """
-        reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, random_state=42)
+        reducer = umap.UMAP(
+            n_neighbors=n_neighbors,
+            min_dist=min_dist,
+            random_state=42
+        )
         embeddings_2d = reducer.fit_transform(self._embeddings)
 
         plt.figure(figsize=(10, 7))
         plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=labels, cmap="Spectral", s=5)
         plt.colorbar(label="Cluster label")
         plt.title("UMAP projection of the clustered embeddings")
-        plt.savefig("umap-proj.png", dpi=300, bbox_inches="tight")
+        plt.savefig("visual.png", dpi=300, bbox_inches="tight")
         plt.close()
     
-    def _cluster_with_kmeans(self, n_clusters: int = 10) -> Tuple[List[torch.Tensor], np.ndarray]:
-        """
-        Clusters the embeddings using KMeans.
-
-        Args:
-            n_clusters (int): The number of clusters to use.
-
-        Returns:
-            Tuple[List[torch.Tensor], np.ndarray]: The centroids and the labels.
-        """
-        X_torch = torch.from_numpy(self._embeddings).float()
-        X_normalized = X_torch / X_torch.norm(dim=-1, keepdim=True)
-        X = X_normalized.cpu().numpy()
-
-        kmeans = MiniBatchKMeans(n_clusters=n_clusters, init="k-means++", max_iter=1000, n_init=10, random_state=0)
-        labels = kmeans.fit_predict(X)
-        centroids = kmeans.cluster_centers_
-
-        return [torch.from_numpy(c) for c in centroids], labels
--- src/clustering_main.py
+++ src/clustering_main.py
@@ -10,12 +10,19 @@ if __name__ == "__main__":
     # command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-bm", "--base_model", type=str, default="ViT-B/32")
-    parser.add_argument("-fm", "--finetuned_model", type=str, default="models/finetuned.pt")
-    parser.add_argument("-md", "--mode", type=str, default="kmeans")
-    parser.add_argument("--n_clusters", type=int, default=10)
-    parser.add_argument("--n_terms", type=int, default=10)
+    parser.add_argument("-fm", "--finetuned_model", type=str, default="models/finetuned-v2.pt")
     parser.add_argument("--use_base_model", action="store_true")
-    
+    parser.add_argument("--method", type=str, default=None)
+    parser.add_argument("--n_terms", type=int, default=10)
+
+    parser.add_argument("--n_clusters", type=int, default=10)
+    parser.add_argument("--eps", type=float, default=0.2)
+    parser.add_argument("--min_samples", type=int, default=5)
+    parser.add_argument("--min_cluster_size", type=int, default=10)
+    parser.add_argument("--reduce_with", type=str, default=None)
+    parser.add_argument("--n_components", type=int, default=32)
+    parser.add_argument("--represent_with", type=str, default="centroid")
+
     args = parser.parse_args()
 
     # 1. initialize the builder
@@ -32,10 +39,16 @@ if __name__ == "__main__":
         finetuned_model_path=finetuned_model,
         dataset=builder()
     )
-    
-    # 3. cluster the artworks
+
+    # 3. perform clustering
     clusterer.cluster(
-        mode=args.mode,
+        method=args.method,
+        reduce_with=args.reduce_with,
+        represent_with=args.represent_with,
+        n_terms=args.n_terms,
         n_clusters=args.n_clusters,
-        n_terms=args.n_terms
+        eps=args.eps,
+        min_samples=args.min_samples,
+        min_cluster_size=args.min_cluster_size,
+        n_components=args.n_components
     )
