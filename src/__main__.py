e25f10d01fc7a359795bfe8c09aede28f4016ee1
giosteh <stea.giovanni.56@gmail.com>
2025-03-14 00:44:33 +0100

    Improved main

--- src/__main__.py
+++ src/__main__.py
@@ -7,38 +7,66 @@ from describewithLLM import Descriptor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import argparse
 import pickle
 
 
+NR_TOPICS_RANGE = range(3, 21)
+METRICS = ["TD", "IEPS", "IEC", "CES"]
 
-def main():
+
+def modeling():
     # 0. experiments setup
-    nr_topics_range = range(3, 21)
-    results = {"kmeans": {True: {}, False: {}}, "birch": {True: {}, False: {}}}
-    metrics = ["TD", "IEPS", "IEC", "CES"]
+    results = {
+        "kmeans": {False: {}, True: {}},
+        "birch": {False: {}, True: {}},
+        "fcmeans": {True: {}}
+    }
     for m in results.keys():
         for r in results[m].keys():
-            for metric in metrics:
+            results[m][r]["topics"] = []
+            for metric in METRICS[:-1]:
                 results[m][r][metric] = []
-    
+
     # 1. running the experiments
-    descriptor = Descriptor()
-    for nr_topics in nr_topics_range:
+    for nr_topics in NR_TOPICS_RANGE:
+        model = TopicModel(nr_topics=nr_topics)
         for m in results.keys():
             for r in results[m].keys():
-                print(f"Experiment with {m.upper()} and UMAP={r}.")
-                tm = TopicModel(nr_topics=nr_topics)
-                _, image_topics, scores = tm.fit(method=m, reduce=r)
-                _, scores["CES"] = descriptor(tm.output_dir, image_topics)
-                for metric in metrics:
+                experiment_name = f"UMAP+{m.upper()}" if r else f"{m.upper()}"
+                print(f"\n<Running {experiment_name} with {nr_topics} topics>")
+
+                _, image_topics, scores = model.fit(method=m, reduce=r)
+                results[m][r]["dir"] = model.output_dir
+                results[m][r]["topics"].append(image_topics)
+                for metric in METRICS[:-1]:
                     results[m][r][metric].append(scores[metric])
-
-    # 2. plotting and saving the results
-    with open("results.pkl", "wb") as f:
+    
+    # 2. saving the results
+    with open("output/results.pkl", "wb") as f:
         pickle.dump(results, f)
-    plot_results(results, nr_topics_range, metrics)
-    plot_summary_table(results, metrics)
 
+def describing():
+    # 0. loading the results
+    with open("output/results.pkl", "rb") as f:
+        results = pickle.load(f)
+
+    # 1. running the experiments
+    descriptor = Descriptor()
+    for m in results.keys():
+        for r in results[m].keys():
+            for image_topics in results[m][r]["topics"]:
+                output_dir = results[m][r]["dir"]
+                _, score = descriptor(output_dir, image_topics)
+                results[m][r][METRICS[-1]].append(score)
+
+    # 2. saving the results
+    with open("output/results.pkl", "wb") as f:
+        pickle.dump(results, f)
+    
+    # 3. plotting the results
+    plot_results(results, NR_TOPICS_RANGE, METRICS)
+    plot_summary_table(results, METRICS)
 
 def plot_results(results, nr_topics_range, metrics):
     """Plots the results.
@@ -49,7 +77,7 @@ def plot_results(results, nr_topics_range, metrics):
         metrics (list): The metrics to plot.
     """
     sns.set_style("whitegrid")
-    colors = ["crimson", "darkorange", "dodgerblue", "seagreen"]
+    colors = ["crimson", "darkorange", "dodgerblue", "seagreen", "darkviolet"]
     
     for metric in metrics:
         plt.figure(figsize=(10, 6))
@@ -64,7 +92,7 @@ def plot_results(results, nr_topics_range, metrics):
         plt.ylabel(metric)
         plt.legend()
         plt.title(f"Trend of {metric} over Number of Topics")
-        plt.savefig(f"{metric}.png", format="png", dpi=300, bbox_inches="tight")
+        plt.savefig(f"output/{metric}.png", format="png", dpi=300, bbox_inches="tight")
         plt.close()
 
 def plot_summary_table(results, metrics):
@@ -93,10 +121,16 @@ def plot_summary_table(results, metrics):
     table.set_fontsize(14)
     table.auto_set_column_width([i for i in range(len(metrics) + 1)])
     plt.title("Summary of Metrics (Mean ± Std Dev)")
-    plt.savefig("summary_table.png", format="png", dpi=300, bbox_inches="tight")
+    plt.savefig("output/summary_table.png", format="png", dpi=300, bbox_inches="tight")
     plt.close()
 
 
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--describe", action="store_true")
+    args = parser.parse_args()
+
+    if not args.describe:
+        modeling()
+    else:
+        describing()
